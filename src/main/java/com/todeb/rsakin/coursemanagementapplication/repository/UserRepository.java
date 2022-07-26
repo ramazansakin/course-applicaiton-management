23cd318263f912ba1ed5798b625e806aacc004ff
Ramazan Sakin <ramazansakin63@gmail.com>
2022-07-26 17:23:21 +0300

    "No EntityManager with actual transaction available for current thread" error fixed

--- src/main/java/com/todeb/rsakin/coursemanagementapplication/repository/UserRepository.java
+++ src/main/java/com/todeb/rsakin/coursemanagementapplication/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.todeb.rsakin.coursemanagementapplication.repository;
 import com.todeb.rsakin.coursemanagementapplication.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -12,6 +13,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername(String username);
 
+    @Transactional
     void deleteByUsername(String username);
 
 }
