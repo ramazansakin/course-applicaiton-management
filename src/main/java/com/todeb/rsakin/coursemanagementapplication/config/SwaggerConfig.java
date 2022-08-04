21a285e9972a8839ddb78e0db894b489b8671a90
Ramazan Sakin <ramazansakin63@gmail.com>
2022-08-04 07:13:08 +0300

    path prefix set

--- src/main/java/com/todeb/rsakin/coursemanagementapplication/config/SwaggerConfig.java
+++ src/main/java/com/todeb/rsakin/coursemanagementapplication/config/SwaggerConfig.java
@@ -7,17 +7,16 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket customDocket(){
+    public Docket customDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.todeb.rsakin.coursemanagementapplication"))
-//                .paths(PathSelectors.regex("/api/.*"))
+                .paths(PathSelectors.regex("/api/.*"))
                 .build().apiInfo(apiInfo());
     }
 
