2be57d2c3d0e623ceb45d9cba0771727601f09bd
Ramazan Sakin <ramazansakin63@gmail.com>
2022-08-10 08:54:23 +0300

    test: put vs patch

--- src/main/java/com/todeb/rsakin/coursemanagementapplication/controller/CourseController.java
+++ src/main/java/com/todeb/rsakin/coursemanagementapplication/controller/CourseController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -75,6 +76,12 @@ public class CourseController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Course could not be updated successfully");
         }
+        return ResponseEntity.status(HttpStatus.OK).body(update);
+    }
+
+    @PatchMapping
+    public ResponseEntity patchCourse(@RequestBody Course course) {
+        Course update = courseService.update2(course);
         return ResponseEntity.status(HttpStatus.OK).body(update);
     }
 
